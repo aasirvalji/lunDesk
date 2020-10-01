316f22da33e98b4484bf23c54bca5e5a4acb8cde
av <45238682+avxlji@users.noreply.github.com>
2020-10-01 13:58:14 -0400

    added search bar

--- src/pages/myDesk/index.js
+++ src/pages/myDesk/index.js
@@ -2,6 +2,33 @@ import React, { useState } from 'react';
 import styles from './index.module.css';
 import Paper from '@material-ui/core/Paper';
 
+import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
 function MyDesk() {
   const [videoQueue, setVideoQueue] = useState([]);
   const [urlInput, setUrlInput] = useState('lol');
@@ -24,6 +51,8 @@ function MyDesk() {
       .then((data) => console.log('This is your data', data.title));
   }
 
+  const classes = useStyles();
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -40,7 +69,23 @@ function MyDesk() {
           </div>
         </div>
         <Paper elevation={10} className={styles.box2}>
-          <input type="text" name="name" value={urlInput} />
+          <Paper component="form" className={classes.root}>
+            <IconButton className={classes.iconButton} aria-label="menu">
+              <YouTubeIcon />
+            </IconButton>
+            <InputBase
+              className={classes.input}
+              placeholder="Search Google Maps"
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+            <IconButton
+              type="submit"
+              className={classes.iconButton}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Paper>
         </Paper>
       </div>
     </>
