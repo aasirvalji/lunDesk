3609f47777482b612749a67af403f9c3d58ac697
av <45238682+avxlji@users.noreply.github.com>
2020-10-01 13:50:10 -0400

    installed material-ui library and updated myDesk layout

--- src/pages/myDesk/index.js
+++ src/pages/myDesk/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css';
+import Paper from '@material-ui/core/Paper';
 
 function MyDesk() {
+  const [videoQueue, setVideoQueue] = useState([]);
+  const [urlInput, setUrlInput] = useState('lol');
+
   function getYTId(url) {
     var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = url.match(regExp);
@@ -12,21 +16,32 @@ function MyDesk() {
     }
   }
 
+  function getVideoName() {
+    fetch(
+      'https://noembed.com/embed?url=https://www.youtube.com/watch?v=x22TJMv2RYo'
+    )
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data.title));
+  }
+
   return (
     <>
-      <div class={styles.wrapper}>
+      <div className={styles.wrapper}>
         <div className={styles.box1}>
-          <div class={styles.videoWrapper}>
+          <div className={styles.videoWrapper}>
+            {getVideoName()}
             <iframe
               width="560"
               height="349"
               src="http://www.youtube.com/embed/x22TJMv2RYo?rel=0&hd=1"
-              frameborder="0"
-              allowfullscreen
+              frameBorder="0"
+              allowFullScreen={true}
             ></iframe>
           </div>
         </div>
-        ksdjksjksd
+        <Paper elevation={10} className={styles.box2}>
+          <input type="text" name="name" value={urlInput} />
+        </Paper>
       </div>
     </>
   );
